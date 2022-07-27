037c85bef1114ce9434670ffd31ed2391209d57c
Dragos Tanta <dragos.tanta@gmail.com>
2022-07-27 23:05:36 +0300

    curs 18

--- src/curs17/StringBuilderExample.java
+++ src/curs17/StringBuilderExample.java
@@ -5,6 +5,8 @@ public class StringBuilderExample {
 	public static void main(String[] args) {
 		reverseSttring("Java");
 		deleteFromString("Eu invat java");	
+		replaceFromString("Salut Bogdan", 6, 12, "Oana");
+		insertIntoString("Salut ", 5, "Ion");
 		
 	}
 
@@ -18,6 +20,19 @@ public class StringBuilderExample {
 		StringBuilder sb = new StringBuilder(text);
 		sb.delete(2, 6);
 		System.out.println(sb);
+	}
+	
+	public static void replaceFromString(String text, int stratIndex, int endIndex, String textToReplace) {
+		StringBuilder sb =  new StringBuilder(text);
+		sb.replace(stratIndex, endIndex, textToReplace);
+		System.out.println(sb);
+		
+	}
+	public static void insertIntoString(String text, int indexStart, String textToInsert) {
+		StringBuilder sb =  new StringBuilder(text);
+		sb.insert(indexStart, textToInsert);
+		System.out.println(sb);
+
 	}
 	
 }
